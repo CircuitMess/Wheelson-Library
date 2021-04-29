e083875f7a1b643a0ac3f3124e0a86e4ab8d8fce
hrvojekolar <hrvojekolar1998@gmail.com>
2021-04-29 16:20:03 +0200

    add extern Nuvoton Nuvo in Wheelson.h

--- src/Wheelson.h
+++ src/Wheelson.h
@@ -1,7 +1,11 @@
 #ifndef WHEELSON_LIBRARY_WHEELSON_H
 #define WHEELSON_LIBRARY_WHEELSON_H
 
+#include "Nuvoton.h"
+
 #define I2C_SDA 14
 #define I2C_SCL 15
+
+extern Nuvoton Nuvo;
 
 #endif //WHEELSON_LIBRARY_WHEELSON_H
