872ff86cf7c11ac85e80930e53bc0bcedb204a2b
Emil Gajšak <emil.gajsak@gmail.com>
2021-04-29 13:04:49 +0200

    Added WheelsonLED class

---
+++ src/WheelsonLED.h
@@ -0,0 +1,25 @@
+#ifndef WHEELSON_LIBRARY_WHEELSONLED_H
+#define WHEELSON_LIBRARY_WHEELSONLED_H
+
+#include <Wire.h>
+#define BACKLIGHT_SET_BYTE 0x20
+#define BACKLIGHT_GET_BYTE 0x21
+#define HEADLIGHT_SET_BYTE 0x22
+#define HEADLIGHT_GET_BYTE 0x23
+
+class WheelsonLED {
+public:
+	WheelsonLED(TwoWire& wire);
+	void setBacklight(bool backlight);
+	bool getBacklight();
+	void setHeadlight(uint8_t intensity);
+	uint8_t getHeadlight();
+
+private:
+	bool backlight = false;
+	uint8_t headlight = 20;
+	TwoWire& Wire;
+};
+
+
+#endif //WHEELSON_LIBRARY_WHEELSONLED_H
