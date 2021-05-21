90a673106bd4f95324d377ba5c6b930f67bc7716
Domagoj <domagoj.zoraja@fer.hr>
2021-05-21 10:51:11 +0200

    implement set&get rgb for i2c interface

--- src/Nuvoton/WheelsonLED.h
+++ src/Nuvoton/WheelsonLED.h
@@ -6,6 +6,12 @@
 #define BACKLIGHT_GET_BYTE 0x21
 #define HEADLIGHT_SET_BYTE 0x22
 #define HEADLIGHT_GET_BYTE 0x23
+#define RGB_SET_BYTE 0x24
+#define RGB_GET_BYTE 0x25
+
+enum RGBColour{
+		OFF, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
+};
 
 class WheelsonLED {
 public:
@@ -14,9 +20,13 @@ public:
 	bool getBacklight();
 	void setHeadlight(uint8_t intensity);
 	uint8_t getHeadlight();
+	void setRGB(RGBColour colour);
+	RGBColour getRGB();
 
 private:
 	TwoWire& Wire;
+
+	RGBColour rgb = OFF;
 };
 
 
