e49e56d2db05912af620fc3507fe8f07b803d4d3
Emil Gajšak <emil.gajsak@gmail.com>
2021-07-15 13:04:46 +0200

    Tweaked battery shutdown voltage

--- src/BatteryService.cpp
+++ src/BatteryService.cpp
@@ -44,7 +44,7 @@ uint16_t BatteryService::getVoltage() const{
 }
 
 uint8_t BatteryService::getPercentage(){
-	int16_t percentage = map(voltage, 3600, 4200, 0, 100);
+	int16_t percentage = map(voltage, 3650, 4200, 0, 100);
 	if(percentage < 0){
 		return 0;
 	}else if(percentage > 100){
